3cfb98238d4a86566df49f7fb03205bb0181f312
Jan Lampert <83476259+janlampert@users.noreply.github.com>
2022-04-10 12:51:40 +0200

    Adding main.py
    Adding the main file

---
+++ ISS.py
@@ -0,0 +1,29 @@
+import requests
+import pyperclip
+
+
+def getLocation():
+    response = requests.get("http://api.open-notify.org/iss-now.json")
+
+    responseJson = response.json()
+    #responseJsonFormatted = json.dumps(responseJson, indent=4, sort_keys=True)
+
+    position = responseJson["iss_position"]
+
+    longitude = position["longitude"]
+    latitude = position["latitude"]
+
+    positionReturn = f"{latitude},{longitude}"
+
+    return positionReturn
+
+def copyToClipboard():
+    position = getLocation()
+    pyperclip.copy(position)
+
+def run():
+    copyToClipboard()
+
+
+
+
